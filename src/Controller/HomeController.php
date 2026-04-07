81136ece946179b56c1f97fbbc09ed84805a127c
Sohaib <somar54@hotmail.fr>
2026-04-07 19:41:22 +0200

    autorization

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,13 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 final class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
-    dump('salut');
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
