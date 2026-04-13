d9ff97829c9c5273a6f29b393ae0f164d0c3f9f5
Sohaib <somar54@hotmail.fr>
2026-04-14 00:26:07 +0200

    affichage de la modal de suppression

--- assets/client.js
+++ assets/client.js
@@ -1,9 +1,10 @@
 document.addEventListener("turbo:load", () => {
     const container = document.getElementById("clients-list");
     const overlay = document.getElementById("modal-overlay");
+    const overlayDelete = document.getElementById("modal-overlay-delete");
     const btnAdd = document.getElementById("btn-add-client");
-    const btnEdit = document.getElementById("btn-add-edit");
     const btnClose = document.getElementById("modal-close");
+    const btnCloseDelete = document.getElementById("modal-close-delete");
     const form = document.getElementById("form-client");
     let currentClientId = null;
 
@@ -22,6 +23,7 @@ document.addEventListener("turbo:load", () => {
                         <th>Téléphone</th>
                         <th>Entreprise</th>
                         <th>Modifier</th>
+                        <th>Supprimer</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -34,6 +36,7 @@ document.addEventListener("turbo:load", () => {
                             <td>${c.phone ?? "—"}</td>
                             <td>${c.company ?? "—"}</td>
                             <td><button class="btn-edit" data-id="${c.id}">Modifier</button></td>
+                            <td><button class="btn-delete" data-id="${c.id}">Supprimer</button></td>
                         </tr>
                     `,
                         )
@@ -49,6 +52,14 @@ document.addEventListener("turbo:load", () => {
                 openEditModal(client);
             });
         });
+
+        document.querySelectorAll(".btn-delete").forEach((btn) => {
+            btn.addEventListener("click", () => {
+                const id = parseInt(btn.dataset.id);
+                const client = clients.find((c) => c.id === id);
+                openDeleteModal(client);
+            });
+        });
     }
 
     loadClients();
@@ -60,10 +71,16 @@ document.addEventListener("turbo:load", () => {
         overlay.classList.remove("hidden");
     });
     btnClose.addEventListener("click", () => overlay.classList.add("hidden"));
+    btnCloseDelete.addEventListener("click", () =>
+        overlayDelete.classList.add("hidden"),
+    );
     overlay.addEventListener("click", (e) => {
         if (e.target === overlay) overlay.classList.add("hidden");
     });
-    //btnEdit.addEventListener("click", () => {});
+
+    overlayDelete.addEventListener("click", (e) => {
+        if (e.target === overlayDelete) overlayDelete.classList.add("hidden");
+    });
 
     // ── Soumission du formulaire ──
     form.addEventListener("submit", async (e) => {
@@ -97,5 +114,11 @@ document.addEventListener("turbo:load", () => {
             client.company ?? "";
         currentClientId = client.id;
         overlay.classList.remove("hidden");
+    }
+
+    function openDeleteModal(client) {
+        document.getElementById("message-delete-client").textContent =
+            "Voulez-vous vraiment supprimer le client " + client.name + " ?";
+        overlayDelete.classList.remove("hidden");
     }
 });
